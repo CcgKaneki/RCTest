bb46ff398918884abeac183e4d3cee1b9867e501
zhangli <1192026016@qq.com>
2021-09-24 16:42:49 +0800

    666

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './example/Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Clock></Clock>
         <p>
           山东滕州现低俗婚闹，一半裸新郎被吊到半空遭高压水枪喷射
         </p>
