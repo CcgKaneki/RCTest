8b57e2cfdd0a7f73f9db51a53a357e72dfc7ded5
zhangli <1192026016@qq.com>
2021-09-26 13:46:11 +0800

    9262

--- src/App.js
+++ src/App.js
@@ -13,34 +13,6 @@ function App() {
           山东滕州现低俗婚闹，一半裸新郎被吊到半空遭高压水枪喷射
         </p>
         
-        <div id="main" style="width: 600px;height:400px;"></div>
-        <script type="text/javascript">
-            // 基于准备好的dom，初始化echarts实例
-            var myChart = echarts.init(document.getElementById('main'));
-
-            // 指定图表的配置项和数据
-            var option = {
-                title: {
-                    text: 'ECharts 入门示例'
-                },
-                tooltip: {},
-                legend: {
-                    data:['销量']
-                },
-                xAxis: {
-                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-                },
-                yAxis: {},
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }]
-            };
-
-            // 使用刚指定的配置项和数据显示图表。
-            myChart.setOption(option);
-        </script>
 
 
         <a
