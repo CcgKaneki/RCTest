125274a0095e32303d94837e9f7c12807877def7
zhangli <1192026016@qq.com>
2021-09-26 13:52:24 +0800

    9266

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './example/Clock';
+import * as echarts from 'echarts';
 
 function App() {
   return (
@@ -12,7 +13,24 @@ function App() {
           山东滕州现低俗婚闹，一半裸新郎被吊到半空遭高压水枪喷射
         </p>
         
-
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+        // 绘制图表
+        myChart.setOption({
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            xAxis: {
+                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
 
         <a
           className="App-link"
